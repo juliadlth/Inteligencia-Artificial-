da236b5fe2999f5a532dbc9969063c7c1da5245a
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-08-27 21:56:27 -0500

    Practica 1

---
+++ Variables.py
@@ -0,0 +1,6 @@
+variable = "Practica 1" #guardamos el mensaje en la variable 
+v1=10                   #le asignamos un valor a la variable 1
+v2=10                   #le asignamos un valor a la variable 2
+r=v1+v2                 #Creamos la variable "r"y le guardamos la suma de la variable 1 y la variable 2
+print("El resultado de la suma es:" , r)     #Imprimimos el resultado que esta guardado en la variable "r"
+
