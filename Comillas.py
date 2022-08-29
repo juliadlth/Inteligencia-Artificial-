4cb89d3e7a258834703a2b38d77d154ebeec2346
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-08-28 22:44:31 -0500

    Practica 2

---
+++ Comillas.py
@@ -0,0 +1,7 @@
+
+Variable1="Las flores son 'azules'"   #Declaramos el mensaje con comillas dobles
+Variable2='Las flores son "azules"'   #Declaramos el mensaje con comillas sencillas 
+print(Variable1),print(Variable2)     #imprimimos ambos mensajes en la consola 
+
+mensaje='"print()"se utiliza para imprimir valores en la consola.' #Creamos un mensaje con comillas
+print(mensaje)                        #Imprimimos el mensaje en la consola 
