69ac76ce90b8ae355ee686fc91412b1cad66d49d
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-08-28 23:37:30 -0500

    Practica 4

---
+++ Title_Upper_Lower.py
@@ -0,0 +1,11 @@
+#Parte1
+nombre="enrique barros fernández"    #Declaramos el nombre en minuculas y lo guardamos en nombre
+print(nombre.title())                #Imprimimos utilizando el metodo "title()"para colocar las mayusculas
+
+#Parte 2
+minu="Esta Es Una Frase Para Ser Formateada."  #Guardamos la frase en minu con letras mayusculas
+print(minu.lower())                   #Imprimimos utilizando el metodo lower para hacer las letras minusculas
+
+#Parte 3
+mayu="Esta Es Una Frase Para Ser Formateada."     #Guardamos la frase en la variable mayu
+print(mayu.upper())                         #Imprimimos utilizando el metodo upper para hacer las letras mayusculas 
