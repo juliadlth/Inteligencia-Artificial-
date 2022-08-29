5cb9f31d8a0cf8c5546e38df91ddd56127fcf7b6
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-08-29 00:39:48 -0500

    Practica 6

---
+++ Operadores.py
@@ -0,0 +1,19 @@
+#Parte1
+suma=20 + 23 + 44                                   #Asignamos los valores integers a la variable suma y los sumamos
+print("El resulado de la suma es:",suma)            #Imprimimos el resultado 
+
+#Parte2
+resta=20 - 23 - 84                                  #Realizamos la resta y la guardamos en la variable resta
+print("El resultado de la resta es:",resta)         #Imprimimos el resultado
+
+#Parte3
+multiplicacion=20 * 23 *1.8914                      #Realizamos la multiplicacion utilizando el operando"*"
+print("El resultado de la multiplicacion es:",multiplicacion) #Imprimimos el resultado 
+
+#Parte 4
+division=5000/230/2                                  #Realizamos la operacion utilizando "/"para hacer la divicion
+print("El resultado de la division es:",division)    #imprimimos el resultado separando el string con una coma
+
+#Parte 5
+operacion=10/5+15-17                                 #Realizamos la operacion utilizando todos los operandos utilizados previamente
+print("El resultado de la operacion es:",operacion)  # Imprimimos el ressultado 
