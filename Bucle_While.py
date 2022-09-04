62c8f88bf93f3bdaa282003fd91dcaae9fb12a8e
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 13:35:11 -0500

    Practica 23

---
+++ Bucle_While.py
@@ -0,0 +1,5 @@
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+x=0 #Establezco mi valor de x 
+while x <= 15: #Creo el bucle donde se cumplira la condicion donde x sea menor o igual a 15
+    print (x) #Imprimo mi bucle 
+    x+=5      #Incremento mi x de 5 en 5 hasta llegar a 15 
