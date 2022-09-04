8e8e4ebed6c2273bc06ee3d31336691c87e6f5fc
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-03 23:21:27 -0500

    Practica 10

---
+++ Listas.py
@@ -0,0 +1,17 @@
+
+#Parte 1
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]  #Establecemos la lista 
+print(colores[3])  #Imprimos la tercera posicion de la lista 
+
+#Parte 2
+print("\nEl color ",colores[0],"se encuentra en la posicion 0"   #Imprimimos los colores con su respectiva posicion
+      "\nEL color",colores[7],"se encuentra en la posicion 7\n")    
+#Parte 3
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2
+
+Lista=["tres","dos","cinco","cuatro","uno"] #Creamos la lista con las posiciones dadas
+print(Lista)  #Imprimimos la lista
