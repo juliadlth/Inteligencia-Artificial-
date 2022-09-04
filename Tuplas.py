6513f4860a3e087d30d21e79f3ca032482cdd785
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 11:59:52 -0500

    Practica 17

---
+++ Tuplas.py
@@ -0,0 +1,9 @@
+#Parte 1
+#Imprime la segunda posición de esta tupla
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja') #Declaro la tupla y sus elementos 
+print(colores[1]) #Imprimo la segunda posicion de la tupla, siendo la numero '1'porque las posiciones inician desde el '0'
+
+#Parte 2
+#Utiliza los símbolos de suma y resta para obtener el resultado 25
+numeros = (10, 1, 5, 11) #Declaramos la tupla y sus elementos 
+print("\n",numeros [0]-numeros[1]+numeros[2]+numeros[3]) #Llamo las posiciones de los numeros que necesito y voy realizando las operaciones, imprimo el resultado 
