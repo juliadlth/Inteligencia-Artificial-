95fdd440c4c47ea138030a404043e5441621fcce
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 12:24:13 -0500

    Practica 19

---
+++ Condicional_If.py
@@ -0,0 +1,26 @@
+#Parte 1
+#Cambia el operador para que la condición sea True.
+num1 = 15 #Establecemos nuestra variable 1
+num2 = 20 #Establecemos nuestra variable 2 
+
+#Analizamos que 20 es mayor que 15 entonces mi num1 es menor que mi num2
+if num1 < num2:  #Ponemos la condicion con '<'
+	print('Se ejecuta el if.') #La condicion es 'True' y por ellos se imprime en consola 
+
+#Parte 2
+#Cambia el operador para que la condición sea True.
+num1 = 1450  #Declaro mi primer variable
+num2 = 60    #Declaro mi segunda variable
+#Analizamos que 1450 es mayor que 60 ,entonces mi num1 es > mi num2
+if num1 > num2: #Cambio el operador para que mi condicion se cumpla
+	print('\n Se ejecuta el if.') #La condicion es 'True' y por ello se imprime 
+
+#Parte 3
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador
+#Como no puedo cambiar de operador el cual cumple la condicion si las variables sean distintas, entonces las declaro iguales
+num1 = 60 #Declaro mi primer variable
+num2 = 60 #Declaro mi segunda variable 
+
+if num1 != num2:#Se declara la condicion
+	print('Se ejecuta el if.')#No se imprime en consola porque da como resultado 'False'
+
