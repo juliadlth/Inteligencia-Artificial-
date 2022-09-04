a7a2c654f535f3c63ee96a5f44b69bb625632324
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 12:08:06 -0500

    Practica 18

---
+++ ConvertirListasyTuplas.py
@@ -0,0 +1,5 @@
+#Convierte la siguiente lista en una tupla y asegúrate que se haya convertido en tupla correctamente
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Declaro la lista y sus elementos 
+tupla=tuple(colores)  #Transformo mi lista colores a una tupla por medio de 'tuple()'
+print(tupla)          #Imprimo mi tupla y ya han cambiado los corchetes a parentesis
+print("\n",type(tupla)) #confirmo por medio de 'type' para ver de que clase es 
