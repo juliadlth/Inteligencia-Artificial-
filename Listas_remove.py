4eb233157af6135b5a6237d4857ba59856fcea8c
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 00:02:29 -0500

    Practica 12

---
+++ Listas_remove.py
@@ -0,0 +1,6 @@
+#Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Declaro la lista con sus elementos 
+colores.remove('amarillo') #utilizo '.remove()'y el color o el elemento que quiero eliminar en el parentesis
+colores.remove('rojo')     #Remuevo el color rojo
+print(colores)             #Imprimo la lista nueva
+
