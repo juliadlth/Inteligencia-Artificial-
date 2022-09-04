79f77a69b7e040b66384ca28e7e8ef2f80dba94a
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 11:47:34 -0500

    Practica 16

---
+++ Listas_sort.py
@@ -0,0 +1,5 @@
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+          'lila', 'negro', 'rosa', 'blanco', 'naranja']      #Declaro la lista y sus elementos 
+colores.sort(reverse=True)     #Declaro la lista colores con 'sort(reverse=True)'para que los elementos se ordenen en orden alfabetico pero empezando de la 'z'
+print(colores)                 #Imprimo la lista ya ordenada 
