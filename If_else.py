5f72f491d2d1f15123529bd06b57f8faa1b3421f
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 12:50:12 -0500

    Practica 20

---
+++ If_else.py
@@ -0,0 +1,14 @@
+#Corrige el siguiente condicional if else
+
+#1-.Primero se establece el condicional if y despues el else
+#2.-El condional else lleva ':' y el if tambien lleva dos puntos 
+#3.-Lo que se va a imprimir en ambos print va entre parentesis y la intruccion print debe empezar en minusculas
+#4.-El color rojo va con comillas 
+#5.-Solo se tiene que establecer una condicion en el 'if'
+
+
+color = "rojo" #Guardo mi elemento con comillas 
+if color != "rojo": #Establezco mi condicion en el 'if' 
+    print("El color no es rojo.") #Imprimo si la condicion no se cumple 
+else:                             #Si la condicion se cumple
+ print("El color es rojo")        #Se imprime en este caso el else porque rojo no es diferente de rojo
