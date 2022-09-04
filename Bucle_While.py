7164861d85e75c208098fb234f70bc5268a5bc35
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 14:00:28 -0500

    Practica 23

---
+++ Bucle_While.py
@@ -0,0 +1,23 @@
+#Parte 1
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+x=0 #Establezco mi valor de x 
+while x <= 15: #Creo el bucle donde se cumplira la condicion donde x sea menor o igual a 15
+    print(x) #Imprimo mi bucle 
+    x+=5      #Incremento mi x de 5 en 5 hasta llegar a 15 
+    
+print("\n") #Doy un salto de linea
+
+#Parte 2
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+x=0 #Inicializo mi x en 0
+while x  >=-100: #Creo el bucle donde se cumplira la condicion donde x sea mayor o igual a -100
+   print(x)   #Imprimo mi bucle
+   x-=20      #Decremento mi x de 20 en 20 hasta llegar a -100
+
+print("\n") #Doy un salto de linea
+
+#Parte 3
+x=10 #Inicializo mi x en 10
+while x>=0: #Creo el bucle donde se cumplira la condicion donde x sea mayor o igual a 0
+    print(x)#Imprimo mi bucle
+    x-=1    #Decremento de mi x de 1 en 1 hasta que es 0 
