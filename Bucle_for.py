dc66714c213b6eaedeb623116ca69371ad48cafb
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 14:47:39 -0500

    Practica 25

---
+++ Bucle_for.py
@@ -0,0 +1,4 @@
+#Crea un bucle for que itere la siguiente tupla 
+colores = ('rojo','azul','verde','amarillo') #Creo la tupla con sus elementos
+for x in colores: #Creo un bucle for para obtenr cada color de la tupla 
+    print('El color es: ' + x + '.') #Imprimo el resultado de bucle for 
