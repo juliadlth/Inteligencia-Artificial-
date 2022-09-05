5f78abf255d10f11945acd97646ce9e3ac970ed9
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 22:35:00 -0500

    Practica 30

---
+++ Funciones.py
@@ -0,0 +1,6 @@
+def suma(numero1, numero2):   #Creamos la funcion suma 
+	print(numero1 + numero2)  #Imprimimos la operacion 
+
+suma(15, 15)                 
+suma(40, 10)                 #Establezco los valores en su posicion correspondiente
+suma(50000, 7000)
