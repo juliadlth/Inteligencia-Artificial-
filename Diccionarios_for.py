09d1c8083c8bff8cc494d8026151c2282308906b
juliadlth <112038442+juliadlth@users.noreply.github.com>
2022-09-04 22:17:23 -0500

    Delete Diccionarios_for.py

--- Diccionarios_for.py
+++
@@ -1,9 +0,0 @@
-teclado1 = {                                         #Se declara el diccionario con sus elementos
-	'Categoría': 'Teclados',
-	'Modelo': 'HyperX Alloy FPS Pro',
-	'Precio': '89,99'
-}
-
-for x in teclado1:                                 #Declaro el for 
-	print(x, '=', teclado1[x] + '.')               #Imprimo el resultado
-
